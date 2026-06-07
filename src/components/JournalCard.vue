89bca6f07e0100845c7a2903ac9c03a0782db199
Faris Hasim <farishasimsyauqi@gmail.com>
2026-06-07 13:05:26 +0700

    update badge color

--- src/components/JournalCard.vue
+++ src/components/JournalCard.vue
@@ -33,10 +33,10 @@ defineProps({
         </div>
         <div class=" w-20 md:w-[15%] grid place-items-center">
             <!-- <h3>Scimago:</h3> -->
-            <Badge v-if="scimago === 'Q1'" class="bg-gradient-to-br from-blue-500 to-red-400" :content="scimago" />
-            <Badge v-else-if="scimago === 'Q2'" class="bg-gradient-to-br from-amber-500 to-yellow-300" :content="scimago" />
-            <Badge v-else-if="scimago === 'Q3'" class="bg-gradient-to-br from-slate-500 to-slate-300" :content="scimago" />
-            <Badge v-else-if="scimago === 'Q4'" class="bg-gradient-to-br from-yellow-900 to-amber-600" :content="scimago" />
+            <Badge v-if="scimago.includes('Q1')" class="bg-gradient-to-br from-blue-500 to-red-400" :content="scimago" />
+            <Badge v-else-if="scimago.includes('Q2')" class="bg-gradient-to-br from-amber-500 to-yellow-300" :content="scimago" />
+            <Badge v-else-if="scimago.includes('Q3')" class="bg-gradient-to-br from-slate-500 to-slate-300" :content="scimago" />
+            <Badge v-else-if="scimago.includes('Q4')" class="bg-gradient-to-br from-yellow-900 to-amber-600" :content="scimago" />
             <Badge v-else class="bg-gradient-to-br from-black to-gray-500" :content="scimago" />
         </div>
         <div v-if="scopus" class="sm:w-1/6 grid place-items-center">
